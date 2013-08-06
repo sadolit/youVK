c4c8784191f509c87bb392db869910f2a62fed76
sadolit <sadolit@gmail.com>
2013-08-06 13:26:09 +0400

    Finally added the possibility to downloand from youtube

--- youVK.py
+++ youVK.py
@@ -1,7 +1,85 @@
+# youVK. Copyright: sadolit, 2013
+
+
+#~/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
 import vkontakte
 import vk_auth
 import requests
 import argparse
+
+from pafy import Pafy
+import os
+from subprocess import check_call
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+""" Download and converts youtube video """
+
+class Downloader():
+    
+    def __init__(self, url, out, author=u"", title=u""):
+        self.url = url
+        self.out = out
+        self.filename = u""
+        self.tmp = u"tmp.mp3"
+        self.author = author 
+        self.title = title
+    
+    def __createVideo(self):
+        self.video = Pafy(self.url)
+
+    def __download(self):
+        best = self.video.getbest(preftype="mp4")
+        self.filename = u"out." + best.extension
+        best.download(progress=True, filepath=self.filename.encode('utf-8'))
+    
+    """ Converts video to mp3 using ffmpeg and 
+    adds tags if they were given"""
+
+    def __convert(self):
+        try:
+            print "\n\t\t\tExtracting audio...\n"
+
+            check_call([u'ffmpeg', u'-v', u'0', u'-i',
+            self.filename, u'-b', u'320K', self.tmp])
+            
+            print "\n\t\t\tAdding tags...\n"
+            
+            check_call([u'ffmpeg', u'-v', u'0', u'-i', self.tmp, u'-b', u'320K',
+            u'-id3v2_version', '3',  
+            u'-metadata', u'title=' + self.title, 
+            u'-metadata', u'artist=' + self.author, self.out])
+        except OSError as e:
+            print '\nError converting file!\n'
+            self.clean()
+            raise e
+
+    """ Removes video and mp3 file """
+    
+    def clean(self):
+        try:
+            os.remove(self.filename)
+        except:
+            print "\nError cleaning!\n"
+        try:
+            os.remove(self.tmp)
+        except:
+            print "\nError cleaning!\n"
+        try:
+            os.remove(self.out)
+        except OSError as e:
+            print "\nError cleaning!\n"
+            raise e
+   
+    def process(self):
+        self.__createVideo()
+        self.__download()
+        self.__convert()
 
 """ Authorizes using given email and password. Returns vk.API object """
 
@@ -47,24 +125,39 @@ def save(vk, fmt_resp):
 
 def parseArgs():  
     parser = argparse.ArgumentParser(description='youVK. author:sadolit')
+    parser.add_argument("-l", "--link", help = "Youtube video link", required = True)
     parser.add_argument('-e', '--email', help = "User email", required = True)
     parser.add_argument('-p', '--password', help = "Password", required =  True)
-    parser.add_argument("-f", "--file", help = "File to upload", required = True)
-    return  parser.parse_args()
+    parser.add_argument("-a", "--author", help = "Author to be added to ID3 tag of the audio", required=False)
+    parser.add_argument("-t", "--title", help = "Title to be added to ID3 tag of the audio", required=False)
+    arg_list = parser.parse_args()
+    return arg_list
 
 def main(args):
+    filename = "out.mp3"
     try:
+        print "\n\t\t\tAuthorizing to vk...\n"
         vk = authorize(args.email, args.password)
     except RuntimeError:
-        print "Authorization error!"
+        print "\nAuthorization error!"
+        return
+    print "\n\t\t\tDone!\n"
+    try:
+        downloader = Downloader(args.link, filename, args.author, args.title)
+        downloader.process()
+    except KeyError:
+        print "\nError downloading video!"
         return
     try:
-        save(vk, parseOutput(uploadAudio(getUploadURL(vk), args.file)))
-    except IOError:
-        print "File not found!"
+        print "\n\t\t\tUploading to vk...\n"        
+        save(vk, parseOutput(uploadAudio(getUploadURL(vk), filename)))
     except Exception:
-        print "Eror uploading file!"
-    print "Done. File " + args.file + " was sent to server!"
+        print "\nError uploading to vk!\n"
+        return
+    print "\n\t\t\tDone!\n"
+    print "\n\t\t\tCleaning...\n"
+    downloader.clean()
+    print "\n\t\t\tDone!\n"
 
 if __name__ == "__main__" :
     args = parseArgs()
